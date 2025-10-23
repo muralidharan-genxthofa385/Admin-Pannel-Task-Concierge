c0345fc6604e1fd0d43a02255904af0843c5d531
muralidharan-genxthofa385 <Muralidharan@genxthofa.com>
2025-10-23 18:53:54 +0530

    created question types

--- src/resources/js/Screens/Questions Screens/QuestionsScreen.tsx
+++ src/resources/js/Screens/Questions Screens/QuestionsScreen.tsx
@@ -2,13 +2,16 @@ import { getAllServicess } from '@/Service/Questions_page_service/Questions_page
 import { themes } from '@/Themes'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
-import { ChevronRight, SquarePen } from 'lucide-react'
+import { ChevronRight, Plus, SquarePen, Trash2 } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import { Card } from '@/components/ui/card'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import FormGroup from '@mui/material/FormGroup'
 interface servicedata{
   name:string,
   id:number
@@ -21,11 +24,18 @@ const QuestionsScreen:React.FC = () => {
   const [Servicedata, setServicedata] = useState<servicedata[]>([])
   const [selectedService,setSelectedService]=useState<servicedata|null>(null)
   const [questionType,setQuestionType]=useState<'text'|'radio'|'checkbox'>('text')
+  const [radioOptions,setRadioOptions]=useState([''])
+  const [openAddradioText,setOpenAddradioText]=useState(false)
+    const [openAddCheckboxText,setOpenAddCheckboxText]=useState(false)
+
+
+
+  // console.log(questionType)
 
  const radiostyles = {
-        radioLabel: { ...themes.lightFont, color: "var(--color-dark-text)" },
-        radioButton: { color: "var(--color-grey)", "&.Mui-checked": { color: "var(--color-purple)" } },
-        checkboxStyle: { color: "var(--color-grey)", "&.Mui-checked": { color: "var(--color-purple)", } }
+        radioLabel: { ...themes.lightFont, color: "var(--color-dark-text)", fontFamily: "Sora, sans-serif", },
+        radioButton: { color: "var(--color-grey)", "&.Mui-checked": { color: "var(--color-purple)", fontFamily: "Sora, sans-serif", } },
+        checkboxStyle: { color: "var(--color-grey)", "&.Mui-checked": { color: "var(--color-purple)", fontFamily: "Sora, sans-serif", } }
     }
   console.log('selected id :',selectedService)
   useEffect(()=>{
@@ -43,6 +53,19 @@ console.log('service fetch error',err)
 fetchServices()
   },[])
 
+const addRadioOption=( )=>{
+
+  // setRadioOptions((prev)=>[...prev,])
+
+}
+
+const addCheckboxOption=( )=>{
+
+setOpenAddCheckboxText(true)
+}
+
+
+  {/**-------------------------------------------------------- Main function return ----------------------------------------------------- */}
   return (
     <>
 <div className='flex flex-col gap-10 '>
@@ -73,22 +96,73 @@ renderInput={(params)=>(
   />
 
 </div>
-  <div>
-  <h1 className='text-2xl flex gap-1 items-center'>2<ChevronRight className='text-[var(--color-purple)]'/> Create your Question</h1>
-  <TextField  label="" sx={{...themes.inputFeildActions.active}} inputProps={{sx:{pl:4}}}  InputLabelProps={{ sx: themes.inputFeildActions.inActive}} variant="standard" fullWidth />
+<div>
+   <h1 className='text-2xl flex gap-1 items-center'>2<ChevronRight className='text-[var(--color-purple)]'/> Create your Question</h1>
+   <TextField  label="" sx={{...themes.inputFeildActions.active}} inputProps={{sx:{pl:4}}}  InputLabelProps={{ sx: themes.inputFeildActions.inActive}} variant="standard" fullWidth />
+
 </div>
+
 <div>
     <h1 className='text-2xl flex gap-1 items-center'>3<ChevronRight className='text-[var(--color-purple)]'/> Select your Question Type</h1>
-     <FormControl>
+     <FormControl
+     onChange={(e) =>
+      setQuestionType(e.target.value as 'text' | 'radio' | 'checkbox')
+    }
+     >
       <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="text" name="radio-buttons-group" value={questionType} >
         <FormControlLabel sx={{...radiostyles.radioLabel}} value="text" control={<Radio sx={{...radiostyles.radioButton}} />} label="Text" />
         <FormControlLabel sx={{...radiostyles.radioLabel}} value="radio" control={<Radio sx={{...radiostyles.radioButton}} />} label="Radio" />
         <FormControlLabel sx={{...radiostyles.radioLabel}} value="checkbox" control={<Radio sx={{...radiostyles.radioButton}} />} label="Checkbox" />
       </RadioGroup>
     </FormControl>
-
-
+    
 </div>
+ 
+  {questionType!=="text"&&<div>
+        <h1 className='text-2xl flex gap-1 items-center'>4<ChevronRight className='text-[var(--color-purple)]'/> Create your Question's Options</h1>
+
+ {questionType=='radio'? <div className='flex flex-col'>
+
+  <FormControl
+     onChange={(e) =>
+      setQuestionType(e.target.value as 'text' | 'radio' | 'checkbox')
+    }
+     >
+      <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="text" name="radio-buttons-group" value={questionType} >
+        <FormControlLabel sx={{...radiostyles.radioLabel}} value="text" control={<Radio sx={{...radiostyles.radioButton}} />} label="Text" />
+       
+      </RadioGroup>
+    </FormControl>
+    <Button sx={{...themes.OutlinedButtonStyle,mt:2}}>+ Add Option</Button>
+ 
+ </div>
+:questionType=='checkbox' ?<div>
+
+<FormGroup sx={{display:"flex"}}>
+  <FormControlLabel control={<Checkbox sx={radiostyles.checkboxStyle} />} label={<div className='flex gap-2'>
+ Label <Trash2 className='w-4 text-red-500' onClick={()=>alert('hai')}/>
+  </div>} />
+  </FormGroup>
+
+      <div>
+        <Button  sx={{...themes.OutlinedButtonStyle,mt:2,width:"100%"}} onClick={addCheckboxOption}><Plus/> Add Option</Button>
+        
+      </div>
+
+</div> :
+<>
+</>
+}
+</div>}
+
+<div className='flex flex-col gap-3'>
+ <h1 className='text-2xl flex gap-1 items-center'>{questionType=="text"?4:5}<ChevronRight className='text-[var(--color-purple)]'/> Is your Question is Madnatory and to be filled ?</h1>
+    <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="text" name="radio-buttons-group" value={questionType} >
+        <FormControlLabel sx={{...radiostyles.radioLabel}} value="yes" control={<Radio sx={{...radiostyles.radioButton}} />} label="Yes" />
+                <FormControlLabel sx={{...radiostyles.radioLabel}} value="no" control={<Radio sx={{...radiostyles.radioButton}} />} label="No" />
+      </RadioGroup>
+</div>
+
 
 </Card>
 
